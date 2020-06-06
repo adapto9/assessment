9e072e523bcfdde1b9a42b48d12aac3cfd0840c8
Chua Hock Yao <chuahockyao@gmail.com>
2020-06-06 19:26:05 +0800

    Added DatabaseUtil class

---
+++ backend/database.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+class DatabaseUtil:
+    DB_LOCATION = "backend/database.db"
+
+    def __init__(self):
+        # Init Attributes
+        self.conn = sqlite3.connect(DatabaseUtil.DB_LOCATION)
+        self.conn.row_factory = lambda cursor, row: list(row)
+        self.cur = self.conn.cursor()
+        self.createTable()
+
+    def createTable(self):
+        self.cur.execute('''
+        CREATE TABLE IF NOT EXISTS 
+        employee(
+            id TEXT PRIMARY KEY,
+            login TEXT NOT NULL UNIQUE, 
+            name TEXT NOT NULL, 
+            salary REAL NOT NULL
+            );''')
+
+    def getAllEmployees(self):
+        self.cur.execute('''
+        SELECT * FROM employee;''')
+        return self.cur.fetchall()
+    
+    def getAllEmployeeLogins(self):
+        self.cur.execute('''
+        SELECT id, login FROM employee;''')
+        return self.cur.fetchall()
+
+    def insertEmployees(self, employeeList):
+        try:
+            self.cur.executemany('''
+            INSERT OR REPLACE INTO employee ('id', 'login', 'name', 'salary') 
+            VALUES (?, ?, ?, ?);''', employeeList)
+            self.conn.commit()
+            return 1
+        except Exception as e:
+            self.conn.rollback()
+            print(e)
+            return 0
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
