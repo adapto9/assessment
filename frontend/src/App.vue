a5cf1a5d4643607f47b2d4b5fbaf2f340181d62b
Chua Hock Yao <chuahockyao@gmail.com>
2020-06-08 23:28:45 +0800

    Added Vuetify, created Navigation component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/upload">Upload</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <Navigation />
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <router-view />
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navigation from "./components/Navigation";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    Navigation
+  },
+  created() {
+    this.$vuetify.theme.dark = true;
+  }
+};
+</script>
---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <router-link to="/">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-view-dashboard</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Dashboard</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link to="/upload">
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-publish</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Upload</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>TechHunt 2020</v-toolbar-title>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
